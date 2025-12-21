1f4da44d33b3408a4cffe0090f460eadbe982e60
ARS-223 <saadirahim2005@gmail.com>
2025-12-21 19:44:30 +0100

    Delete project taskmanager directory

--- project taskmanager/model/attribute_model.py
+++
@@ -1,6 +0,0 @@
-class AttributeModel:
-    def __init__(self, name, data_type, primary_key=False, flags=None):
-        self.name = name
-        self.data_type = data_type
-        self.primary_key = primary_key
-        self.flags = flags or []
